1c0183551cef0f73f7fa7ff7b6ecc977eb8ead46
mazurenka <an.drej@mail.ru>
2022-05-06 15:05:15 +0200

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Items} from "./components/Items";
-import {v1} from 'uuid';
 
 export type ItemType = {
     id: string,
@@ -17,113 +16,67 @@ export class App extends React.Component {
     constructor(props: any) {
         super(props)
         this.state = {
-            orders:[],
+            orders: [],
             items: [{
-                    id: 1,
-                    title: 'Chair grey',
-                    img: 'chair_grey.jpeg',
-                    desc: 'abcdefg',
-                    category: 'chairs',
-                    price: '49.99'
-                }, {
-                    id: 2,
-                    title: 'Table',
-                    img: 'table.jpeg',
-                    desc: 'abcdefg',
-                    category: 'tables',
-                    price: '149.99'
-                }, {
-                    id: 3,
-                    title: 'Sofa',
-                    img: 'sofa.jpeg',
-                    desc: 'abcdefg',
-                    category: 'sofas',
-                    price: '349.99'
-                }, {
-                    id: 4,
-                    title: 'Lamp',
-                    img: 'lamp.jpeg',
-                    desc: 'abcdefg',
-                    category: 'lamps',
-                    price: '59.99'
-                }, {
-                    id: 5,
-                    title: 'TV',
-                    img: 'tv.jpeg',
-                    desc: 'abcdefg',
-                    category: 'TVs',
-                    price: '1149.99'
-                }]}
+                id: 1,
+                title: 'Chair grey',
+                img: 'chair_grey.jpeg',
+                desc: 'abcdefg',
+                category: 'chairs',
+                price: '49.99'
+            }, {
+                id: 2,
+                title: 'Table',
+                img: 'table.jpeg',
+                desc: 'abcdefg',
+                category: 'tables',
+                price: '149.99'
+            }, {
+                id: 3,
+                title: 'Sofa',
+                img: 'sofa.jpeg',
+                desc: 'abcdefg',
+                category: 'sofas',
+                price: '349.99'
+            }, {
+                id: 4,
+                title: 'Lamp',
+                img: 'lamp.jpeg',
+                desc: 'abcdefg',
+                category: 'lamps',
+                price: '59.99'
+            }, {
+                id: 5,
+                title: 'TV',
+                img: 'tv.jpeg',
+                desc: 'abcdefg',
+                category: 'TVs',
+                price: '1149.99'
+            }]
+        }
         this.addToOrder = this.addToOrder.bind(this)
     }
 
     render() {
-        /* let itemId = v1()
-
-         let [items, setItems] = useState<Array<ItemType>>([
-             {
-                 id: itemId,
-                 title: 'Chair grey',
-                 img: 'chair_grey.jpeg',
-                 desc: 'abcdefg',
-                 category: 'chairs',
-                 price: '49.99'
-             }, {
-                 id: itemId,
-                 title: 'Table',
-                 img: 'table.jpeg',
-                 desc: 'abcdefg',
-                 category: 'tables',
-                 price: '149.99'
-             }, {
-                 id: itemId,
-                 title: 'Sofa',
-                 img: 'sofa.jpeg',
-                 desc: 'abcdefg',
-                 category: 'sofas',
-                 price: '349.99'
-             }, {
-                 id: itemId,
-                 title: 'Lamp',
-                 img: 'lamp.jpeg',
-                 desc: 'abcdefg',
-                 category: 'lamps',
-                 price: '59.99'
-             }, {
-                 id: itemId,
-                 title: 'TV',
-                 img: 'tv.jpeg',
-                 desc: 'abcdefg',
-                 category: 'TVs',
-                 price: '1149.99'
-             }
-         ])
-
-         let [odrer, setOrder] = useState<Array<ItemType>>([])
-
-         function addToOrder(id: string, title: string, img: string, desc: string, category: string, price: string) {
-             let newItemId = v1()
-             let newItem: ItemType = {id: newItemId, title, img, desc, category, price}
-             debugger
-             setOrder({
-                 ...items,
-                 [newItemId]: [newItem]
-             })
-             debugger
-         }*/
-
         return (
             <div className={'wrapper'}>
                 <Header orders={this.state.orders}/>
 
-                <Items items={this.state.items} anAdd={this.addToOrder} />
+                <Items items={this.state.items} anAdd={this.addToOrder}/>
 
                 <Footer/>
             </div>
         );
     }
+
     addToOrder(item: ItemType) {
-        this.setState({orders: [...this.state.orders, item]})
+        let isInArray = false
+        this.state.orders.forEach((el: ItemType) => {
+            if (el.id === item.id)
+                isInArray = true
+        })
+        if (!isInArray)
+            this.setState({orders: [...this.state.orders, item]})
     }
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,22 @@ import React, {useState} from "react";
 import {FaShoppingCart} from 'react-icons/fa';
 import {Order} from "./Order";
 
+
+
+const showOrders = (props: any) => {
+    return (<div>
+        {props.orders.map((el: any) => (
+            <Order key={el.id} item={el}/>
+        ))}
+    </div>)
+}
+
+const showNothing = () => {
+    return (<div className={'empty'}>
+        <h2>No items</h2>
+    </div>)
+}
+
 export default function Header(props: any) {
 
     let [cartOpen, setCartOpen] = useState<boolean>(false)
@@ -21,9 +37,8 @@ export default function Header(props: any) {
 
                 {cartOpen && (
                     <div className='shop-cart'>
-                        {props.orders.map((el: any) => (
-                            <Order key={el.id} item={el}/>
-                        ))}
+                        {props.orders.length > 0 ?
+                            showOrders(props) : showNothing()}
                     </div>)}
 
             </div>
